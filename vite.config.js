59bbb868c5eb71f77b6ba9949199bd7bba366ddd
dekjul <210040076@stikom-bali.ac.id>
2025-02-14 16:41:15 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   theme: {
-    base: "./",
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
